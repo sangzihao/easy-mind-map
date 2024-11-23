055c5e88c152212b5423f21c7c41e039d5041172
zihao <15139436516@163.com>
2024-11-23 23:56:34 +0800

    delete unused files; complete base function

--- main.ts
+++ main.ts
@@ -1,146 +1,16 @@
 /* eslint-disable no-mixed-spaces-and-tabs */
-import { Emoji } from './emoji';
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Plugin } from 'obsidian';
 // Remember to rename these classes and interfaces!
 import { mindMapHanler } from 'mindmap';
 
 
-interface EasyMindMapPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: EasyMindMapPluginSettings = {
-	mySetting: 'default'
-}
-
 export default class MyPlugin extends Plugin {
-	settings: EasyMindMapPluginSettings;
 
 	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		this.registerMarkdownPostProcessor((element, context) => {
-			const codeblocks = element.querySelectorAll("code");
-
-			for (let index = 0; index < codeblocks.length; index++) {
-				console.log(codeblocks[index])
-				const codeblock = codeblocks.item(index);
-				const text = codeblock.innerText.trim();
-				const isEmoji = text[0] === ":" && text[text.length - 1] === ":";
-
-				if (isEmoji) {
-					console.log(isEmoji)
-					context.addChild(new Emoji(codeblock, text));
-				}
-			}
-		});
-
 		this.registerMarkdownCodeBlockProcessor("mindmap", mindMapHanler);
 	}
 
 	onunload() {
 
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const { contentEl } = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const { contentEl } = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const { containerEl } = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
 	}
 }
--- mindmap.ts
+++ mindmap.ts
@@ -1,37 +1,59 @@
+/* eslint-disable no-mixed-spaces-and-tabs */
 import { MarkdownPostProcessorContext } from "obsidian";
 import { Transformer } from "markmap-lib";
 import { Markmap, loadCSS, loadJS } from "markmap-view";
-import * as markmap from "markmap-view"
+import * as markmap from "markmap-view";
 import { Toolbar } from "markmap-toolbar";
 
-const transformer: Transformer = new Transformer()
+const transformer: Transformer = new Transformer();
 
 /**
- * 思维导图代码块处理函数
- * @param source 代码块中的待渲染的文本
- * @param element HTMLElement，表示代码块的html元素，可以通过该元素创建新标签展示文本
- * @param ctx 文档的上下文信息，包含了文档的信息和待渲染代码块的位置信息等
+ * codeblock handler of mindmap
+ * @param source markdown text before transformation
+ * @param element HTMLElement codeblock tag
+ * @param ctx the context of obsidian documents, including position of document and HTMLElement
  */
-export function mindMapHanler(source: string, element: HTMLElement, ctx: MarkdownPostProcessorContext) {
-  const {root, features} = transformer.transform(source)
-  console.log("features: ", features)
-  // 2. get assets
-  // either get assets required by used features
-  const { styles, scripts } = transformer.getUsedAssets(features);
-  console.log("styles: ", styles)
-  console.log("script: ", scripts)
-  if (styles) {
-    loadCSS(styles)
-  }
-  if (scripts) {
-    loadJS(scripts, {
-      getMarkmap: () => markmap
-    })
-  }
-  const svgElement: SVGSVGElement = element.createSvg("svg", {
-    attr: {
-      "style":"width: 100%; height:100%"
-    },
-  })
-  Markmap.create(svgElement, undefined, root)
+export function mindMapHanler(
+	source: string,
+	element: HTMLElement,
+	ctx: MarkdownPostProcessorContext
+) {
+	// 1. Transform Markdown to data used by markmap
+	const { root, features } = transformer.transform(source);
+	// 2. get assets
+	const { styles, scripts } = transformer.getUsedAssets(features);
+	if (styles) {
+		loadCSS(styles);
+	}
+	if (scripts) {
+		loadJS(scripts, {
+			getMarkmap: () => markmap,
+		});
+	}
+	const svgElement: SVGSVGElement = element.createSvg("svg", {
+		attr: {
+			style: "width: 100%; height:100%",
+		},
+	});
+	// 创建markmap
+	const mm = Markmap.create(svgElement, undefined, root);
+
+	// 创建 markmap工具栏
+	const toolbar: Toolbar = new Toolbar();
+	// 绑定工具栏到markmap上
+	toolbar.attach(mm);
+	// 设置toolbar中的div在一行展示
+	toolbar.el.setCssProps({
+		display: "flex",
+		"justify-content": "center",
+		"align-items": "center",
+	});
+
+	// 创建div用于存放工具栏
+	const toolbarDiv: HTMLElement = element.createDiv({
+		attr: {
+			style: "width: 100%; height:100%;",
+	  },
+	});
+	toolbarDiv.append(toolbar.render());
 }
